fbfc8e6140e113be54efc65377c475edb7f8068c
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-03-08 06:49:52 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for num in range(100):
     if (num // 10) != (num % 10) and (num // 10) < (num % 10):
         print("{}{}".format((num // 10), (num % 10)), end="")
-        if (num != 98):
+        if (num != 89):
             print(", ", end="")
-
 print("")
