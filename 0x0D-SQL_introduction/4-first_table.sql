761b27e907c92fa59de16f3a4349403e938d894d
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-05-16 15:46:50 -0700

    tasks 4 - 7

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- script to creates a table; must not fail if table already exist
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints the full description of the table first_table from the database
+-- the database name will be passed as argument
+SHOW CREATE TABLE first_table;
