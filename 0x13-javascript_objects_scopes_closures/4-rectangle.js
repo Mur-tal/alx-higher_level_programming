8c6029f623ddd35c24c90fd8df02e07db505bfb9
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-06-14 13:27:33 -0700

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let cha = '';
+      for (let j = 0; j < this.width; j++) {
+        cha += 'X';
+      }
+      console.log(cha);
+    }
+  }
+
+  rotate () {
+    let temp = 0;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
