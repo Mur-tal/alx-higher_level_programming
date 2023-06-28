a2089bc722e030edfbb58b9bdb55f2eb4915b731
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-06-28 15:15:28 -0700

    Task #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# sends a request to the URL at $1 and displays the size of the body of the response
 curl -s "$1" | wc -c
