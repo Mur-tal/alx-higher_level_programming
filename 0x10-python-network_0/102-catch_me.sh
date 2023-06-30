84a22c1b398ec932efdce00101eed62c4fccc15d
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-06-30 02:11:53 -0700

    Task #102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me and respond
-curl -sXLH PUT "Origin: MuritalaSchool" "0.0.0.0:5000/catch_me" -d "user_id=98"
+curl -sX PUT -HL "Origin: HolbertonSchool" "0.0.0.0:5000/catch_me" -d "user_id=98"
