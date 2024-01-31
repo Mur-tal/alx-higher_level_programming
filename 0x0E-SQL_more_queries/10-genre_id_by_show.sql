78ce24369824b857bcaa31c59ba13c0a35d3d359
Mur-tal <118113403+Mur-tal@users.noreply.github.com>
2024-01-31 17:25:52 +0100

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++
@@ -1,8 +0,0 @@
--- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
--- Each record should display: tv_shows.title - tv_show_genres.genre_id
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- You can use only one SELECT statement
-SELECT a.title, b.genre_id
-FROM tv_shows a, tv_show_genres b
-WHERE b.show_id = a.id
-ORDER BY a.title ASC, b.genre_id ASC;
--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++
@@ -1,9 +0,0 @@
--- script that lists all shows contained in the database hbtn_0d_tvshows.
--- Each record should display: tv_shows.title - tv_show_genres.genre_id
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- If a show doesn’t have a genre, display NULL
--- You can use only one SELECT statement
-SELECT a.title, b.genre_id
-FROM tv_shows a
-LEFT JOIN tv_show_genres b ON a.id = b.show_id
-ORDER BY a.title ASC, b.genre_id ASC;
