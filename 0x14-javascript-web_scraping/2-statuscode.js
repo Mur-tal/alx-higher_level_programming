69b63d999fcb1f65e6e82990f8265030401f7b51
Mur-tal <118113403+Mur-tal@users.noreply.github.com>
2024-01-31 17:27:08 +0100

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// script that display the status code of a GET request.
-require('request').get(process.argv[2], function (err, x) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + x.statusCode);
-  }
-});
